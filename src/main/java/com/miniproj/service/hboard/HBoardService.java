c94a70e92beac5f1e14372203e62e7cb8ba773fe
pgyseven <pgyseven@naver.com>
2024-07-10 12:05:47 +0900

    feat: 게시판 전체목록 출력 기능을 위한 service 단 구현

--- src/main/java/com/miniproj/service/hboard/HBoardService.java
+++ src/main/java/com/miniproj/service/hboard/HBoardService.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import com.miniproj.model.HBoardVO;
 
-public interface HBoardService 
-{
+public interface HBoardService   //클래스와 동급이다 클래스를 디자인하기 위해서 먼저 만드는것 중괄호 없는 추상메서드만 만들수 있음 추상클래스와 인터페이스도 상속 가능 
+{ // 추상 형체가 없다 이것도 저것도 될 수 있다. 자바의 부모는 object다 이것도 형체가 없다.  여기를 상속 받아서 bean grahp 처럼 구현 부모가 뭐냐에 따라 ! 부모가 동물이냐 식물이냐에 따라 자식이 결정되듯
    // 게시판 전체 리스트 조회
    public List<HBoardVO> getAllBoard();
    
--- src/main/java/com/miniproj/service/hboard/HBoardServiceImpl.java
+++ src/main/java/com/miniproj/service/hboard/HBoardServiceImpl.java
@@ -2,14 +2,31 @@ package com.miniproj.service.hboard;
 
 import java.util.List;
 
-import com.miniproj.model.HBoardVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.miniproj.controller.hboard.HBoardController;
+import com.miniproj.model.HBoardVO;
+import com.miniproj.persistence.HBoardDAO;
+
+@Service // 아래의 클래스가 서비스 객체임을 컴파일러에 공지 (알려준다)
 public class HBoardServiceImpl implements HBoardService {
 
-   @Override
-   public List<HBoardVO> getAllBoard() {
-      // TODO Auto-generated method stub
-      return null;
-   }
+	private static Logger logger = LoggerFactory.getLogger(HBoardServiceImpl.class);
+	
+	@Autowired
+	private HBoardDAO bDao;
+	
+	@Override
+	public List<HBoardVO> getAllBoard() {
+		logger.info("HBoardServiceImpl.........");
+		
+		
+		// DAO 단 호출
+		List<HBoardVO> lst = bDao.selectAllBoard();
+		return lst;
+	}
 
 }
