80c50abe8112f19a762122e780e089dca6ec78b0
pgyseven <pgyseven@naver.com>
2024-07-11 16:55:15 +0900

    하다맘

--- src/main/java/com/miniproj/controller/hboard/HBoardController.java
+++ src/main/java/com/miniproj/controller/hboard/HBoardController.java
@@ -15,6 +15,13 @@ import com.miniproj.model.HBoardDTO;
 import com.miniproj.model.HBoardVO;
 import com.miniproj.service.hboard.HBoardService;
 
+// Controller 단에서 해야 할 일
+// 1) URI 매핑(어떤 URI가 어떤 방식으로(GET/Post)으로 호출 되었을 때 어떤 메서드에 매핑 시킬 것이냐
+// 2) 있다면 view 단에서 보내준 매개변수 수집
+// 3) 데이터베이스에 대한 CRUD 를 수행하기 위해 service단의 해당 메서드를 호출. service단에서 rerutn 갑을 view 바인딩 view 호출
+// 4) 부가적으로... 컨트롤러 단은 servlet에 의해 동작되는 모듈이기 때문에 HttpServletRequest, HttpServletReponse,HttpSession
+// 등의 Servlet 객체들을 이용할 수 있다 -> 이러한 객체들을 이용하여 구현할 기능이 있다면 그 기능은 Controller 단에서 구현한다.(참고로 리퀘스트는 한번 하면 사라지지만 세션은 유저마다 하나씩 생긴다. 유저가 떠날때까지 유지) 서비스단 이후는 서블릿이 아니라 리퀘스트 세션 불러올수 없다 그래서 컨트롤러 단에서만 이걸 불러오는건 다 해야한다.
+
 @Controller // 아래의 클래스가 컨트롤러 객체임을 명시
 @RequestMapping("/hboard")
 public class HBoardController 
@@ -58,7 +65,12 @@ public class HBoardController
 	@RequestMapping(value="/saveBoard", method = RequestMethod.POST)  /*url 호출이름이 위와 같은데 전송방식이 다르기 때문에 문제 없다*/
    public void saveBoard(HBoardDTO boardDTO) {
 		 System.out.println("이게시글을 출력하자...................." + boardDTO.toString()); //"/saveBoard 보이드는 이거에 의해서 .jsp 가 붙어서 간다.
-		 
+		 try {
+			service.saveBoard(boardDTO);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 			// VO는 리드 온리로서 디비에 꺼내온걸 저장해서 앞단까지 가져가는 것 즉 뷰단까지 즉 DTO와 VO가 다른거다 같이 쓰기도 하는데 앞단 뷰단은 자주 바뀔수 있다. 그래서 따로 쓰는게 좋다. 
 	}
   
