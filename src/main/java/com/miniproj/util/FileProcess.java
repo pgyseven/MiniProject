d3493bc4597518df317d6215d5ba669f5df303b3
pgyseven <pgyseven@naver.com>
2024-07-16 16:46:42 +0900

    feat: 중복된 파일이 있다면 파일 이름변경, 이미지 파일인지 아닌지 검사, 이미지 파일이 아니라면 실제로 하드디스크에 저장

--- src/main/java/com/miniproj/util/FileProcess.java
+++ src/main/java/com/miniproj/util/FileProcess.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.Calendar;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,7 +15,7 @@ import com.miniproj.model.BoardUpFilesVODTO;
 public class FileProcess {
 	
 	// file을 realpath에 저장하는 메소드
-	public BoardUpFilesVODTO saveFileToRealPath(byte[] upfile, String realPath, String ext, String originalFileName, long fileSize)  {
+	public BoardUpFilesVODTO saveFileToRealPath(byte[] upfile, String realPath, String contentType, String originalFileName, long fileSize) throws IOException  {
 		BoardUpFilesVODTO result = null;
 		
 
@@ -26,8 +27,103 @@ public class FileProcess {
 		makeDirectory(realPath, ymd);
 		
 		
-		return result;
+		String saveFilePath = realPath + ymd[ymd.length - 1]; //실제 파일의 저장 경로
+		
+		String newFileName = null;
+		String ext = originalFileName.substring(originalFileName.lastIndexOf(".") + 1);
+		if(fileSize > 0) {
+			if(checkFileExist(saveFilePath, originalFileName)) { // 파일 이름이 중복 되는지 검사 중복 된다면, 파일 이름 변경
+				newFileName = renameFileName(originalFileName);
+				
+				
+				
+			}else {
+				newFileName = originalFileName;
+			}
+			
+			
+			if(ImageMimeType.isImage(ext)) {
+				//이미지 파일임 -> 썸네일 이미지, base64 문자열을 만들고 이미지와 함께 저장해야 한다.
+			}else {
+				// 이미지 파일이 아니다. 그냥 현재 파일만 하드디스크에 저장하면 된다.
+				File saveFile = new File(saveFilePath + File.separator + newFileName);
+				FileUtils.writeByteArrayToFile(saveFile, upfile); // 실제 파일 저장 / pom.xml 에 라이브러리 추가해서 메이븐에 coomons fileupload 1.3.3 jar 
+				
+				
+				result = BoardUpFilesVODTO.builder()
+						.ext(contentType)
+						.newFileName(newFileName)
+						.originalFileName(originalFileName)
+						.size(fileSize)
+						.build(); //객체가 만들어진다.
+				
+			}
+			
+		}
+		
+		
+		
+		return result; //저장된 파일의 정보를 담은 객체
 	}
+	
+	
+	
+	
+	
+	
+	
+	// 파일 이름 바꾸는 메서드 
+	// 예 : originalFileName_timestamp.확장자 타임스탬프는 1970년 부터 지금까지 흘러온 시간을 정수화 한거!
+	private String renameFileName(String originalFileName) {
+		String timestamp = System.currentTimeMillis() + "";    // ""이걸로 문자화 long은 정수다 시스템은 컴퓨터 자체다.
+		
+		String ext = originalFileName.substring(originalFileName.lastIndexOf(".") + 1); //substring(a,b) a이상 b 미만 한개만 쓰면 거기 부터 끝까지
+		String fileNameWithOutExt = originalFileName.substring(0, originalFileName.lastIndexOf(".")); // .는 미만이니깐 포함 안됨
+		
+		String newFileName = fileNameWithOutExt + "_" + timestamp + "." + ext;
+		System.out.println("올드 파일 네임 : " + originalFileName ); //new.png
+		System.out.println("새로운 파일 이름 :" + newFileName); //new_1721111043830.png
+		return newFileName;
+	
+	}
+
+
+
+
+
+
+
+
+	// originalFileName 이 saveFilePath 에 존재하는지 안하는지..(파일 중복 여부) / 이렇게 안하고 인덱스 오브를 쓰면 된다 근데 만들었으니 그냥 쓴다.
+	// 중복된 파일이 있다며 true, 없다면 false
+	private boolean checkFileExist(String saveFilePath, String originalFileName) {
+		File tmp = new File(saveFilePath);
+		
+		boolean isFind = false;
+		for(String name : tmp.list()) { //tmp.list()하나의 배열이다
+			if(name.equals(originalFileName)) {
+				System.out.println("이름이 같은게 있다.");
+				isFind = true;
+				break;
+			}
+		
+		
+		}
+		
+		if(!isFind) {
+			System.out.println("이름이 같은 파일이 없습니다.");
+		}
+		return isFind;
+	}
+
+
+
+
+
+
+
+
+
 	// 파일이 저장될 경로의 디렉토리 구조를 "/년/월/일" 형태로 만드는 메서드
 	private String[] makeCalendarPath(String realPath) {
 		Calendar now = Calendar.getInstance();  // 현재 날자 시간 객체
