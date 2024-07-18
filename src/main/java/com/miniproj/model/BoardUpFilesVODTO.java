8008b04e8cfcbb7de34dafc67aa4bc64428bcb40
pgyseven <pgyseven@naver.com>
2024-07-18 11:36:08 +0900

    테스트 위해  buffer로 바꿨다가 원복

--- src/main/java/com/miniproj/model/BoardUpFilesVODTO.java
+++ src/main/java/com/miniproj/model/BoardUpFilesVODTO.java
@@ -22,7 +22,7 @@ public class BoardUpFilesVODTO {
 	private String ext;
 	private long size;
 	private int boardNo;
-	private String base64Img;
+	private String base64Img; //대용량을 빠르게 처리하기 위해서 지금 큰 이미지의 경우 오류가 나거나 파일 명이 안뜸
 	
 
 }
