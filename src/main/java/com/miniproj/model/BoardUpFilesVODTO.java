2b1482de4e82a1444c9e5ee12730b27ae5866bc0
pgyseven <pgyseven@naver.com>
2024-07-25 17:32:01 +0900

    feat: 게시물 수정시 첨부파일의 상태를 기록하는 변수 추가

--- src/main/java/com/miniproj/model/BoardUpFilesVODTO.java
+++ src/main/java/com/miniproj/model/BoardUpFilesVODTO.java
@@ -23,6 +23,6 @@ public class BoardUpFilesVODTO {
 	private long size;
 	private int boardNo;
 	private String base64Img; //대용량을 빠르게 처리하기 위해서 지금 큰 이미지의 경우 오류가 나거나 파일 명이 안뜸
-	
-
+	private BoardUpFileStatus fileStatus; // 게시물 수정시 첨부 파일의 상태를 기록하는 변수(ex : insert ->  새로 저장된 파일, delete->삭제될 파일)
+    // 이넘 타입도 널 가능하다.
 }
